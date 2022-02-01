4e794ed9e88a25b724c6086b591038557ab281e0
2022project <ghkdsigm3@gmail.com>
2022-02-02 00:39:06 +0900

    provide,inject 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-    <MyBtn >BANANA</MyBtn>
-    <MyBtn v-bind:color="color">BANANA</MyBtn>
-    <MyBtn large color="orange" >APPLE</MyBtn>
-    <MyBtn >BANANA</MyBtn>
-    <MyList></MyList>
-    <MyTab :tabList="tabList">
-      <template v-slot:tabPanel-1> Content 1 </template>
-      <template v-slot:tabPanel-2> Content 2 </template>
-      <template v-slot:tabPanel-3> Content 3 </template>
-      <template v-slot:tabPanel-4> Content 4 </template>
-    </MyTab>
+    <button @click="message ='good?'">click</button>
+    <h2>app{{message}}</h2>
+    <Parent />    
 </template>
 
 <script>
-import MyBtn from '~/components/MyBtn'
-import MyList from '~/components/MyList'
-import MyTab from '~/components/MyTab'
+import Parent from '~/components/Parent'
+import { computed } from 'vue'
 
 export default { 
     components:{
-        MyBtn,
-        MyList,
-        MyTab
+        Parent
     },
     data(){
         return{
-            color:'#000',
-            tabList: ["Tab 1", "Tab 2", "Tab 3", "Tab 4"],
+            message:'Hello World',
+           
+        }
+    },
+    provide(){
+        return{
+            msg:computed(() => {
+                return this.message
+            })
         }
     }
 }
