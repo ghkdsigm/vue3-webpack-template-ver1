54f974c85907858ed056d713002167079df35171
2022project <ghkdsigm3@gmail.com>
2022-02-02 00:18:05 +0900

    탭생성 커밋

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,30 @@
     <MyBtn v-bind:color="color">BANANA</MyBtn>
     <MyBtn large color="orange" >APPLE</MyBtn>
     <MyBtn >BANANA</MyBtn>
+    <MyList></MyList>
+    <MyTab :tabList="tabList">
+      <template v-slot:tabPanel-1> Content 1 </template>
+      <template v-slot:tabPanel-2> Content 2 </template>
+      <template v-slot:tabPanel-3> Content 3 </template>
+      <template v-slot:tabPanel-4> Content 4 </template>
+    </MyTab>
 </template>
 
 <script>
 import MyBtn from '~/components/MyBtn'
+import MyList from '~/components/MyList'
+import MyTab from '~/components/MyTab'
 
 export default { 
     components:{
-        MyBtn
+        MyBtn,
+        MyList,
+        MyTab
     },
     data(){
         return{
-            color:'#000'
+            color:'#000',
+            tabList: ["Tab 1", "Tab 2", "Tab 3", "Tab 4"],
         }
     }
 }
---
+++ src/components/MyList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="list">
+    <li v-for="fruit in fruits" :key="fruit">
+      {{fruit}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      fruits : ['banana','apple','cherry','mellon']
+    }
+  }  
+}
+</script>
