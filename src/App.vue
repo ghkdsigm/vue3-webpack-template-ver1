41101cc99e0651ddc92fa89cfcdb854cee0819ad
2022project <ghkdsigm3@gmail.com>
2022-02-01 19:22:46 +0900

    v-if, v-show 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-    <button v-on:click="add">add</button>
-    <h1>{{print}}</h1>
-    <h1>{{msg}}</h1>
-    <button v-on:click="changtxt">changtxt</button>
-    <h2 v-bind:class="{ active: isActive }" @click="activate">클릭</h2>
-    <h3 v-bind:style="{color,fontSize}" @click="trans">클릭</h3>
-    <h3 v-bind:style="fontStyle">클릭</h3>
-    <h3 v-bind:style="[fontStyle,backgroundStyle]">클릭</h3>
+    <button @click="handler">클릭</button>
+    <template v-if="isShow">
+        <ul>
+            <li>1</li>
+            <li>2</li>
+        </ul>
+    </template>
+    <!--
+    <div v-show="isShow">
+        <ul>
+            <li>1</li>
+            <li>2</li>
+        </ul>
+    </div>
+    -->
 </template>
 
 <script>
@@ -15,55 +22,18 @@ import Fruits from '~/components/Fruits'
 export default {
     data(){
         return {
-            msg:'Hello',
-            print:'hey whatup',
-            isActive: false,
-            color: 'red',
-            fontSize: '30px',
-            fontStyle: {
-                color:'orange',
-                fontSize: '20px'
-            },
-            backgroundStyle: {
-                backgroundColor:'gray'
-            },
+           isShow: true,
+           count:0
         }
     },
-    computed:{
-        printed : {
-            get(){
-                return this.print.split('').reverse().join('')
-            },
-            set(value){
-                this.print = value
-            }
-        }
-    },
-    watch:{
-        msg(){
-            console.log('msg', this.msg)
-        }
-    },
-    methods:{
-        add() {
-            this.printed += '!!'
-        },
-        changtxt(){
-            this.msg = 'good!!'
-        },
-        activate(){
-            this.isActive = true
-            
-        },
-        trans (){
-            this.color = 'blue',
-            this.fontSize = '100px'
+    methods:{        
+        handler (){
+            this.isShow = !this.isShow
+            this.count += 1
         }   
     }
 }
 </script>
 
 <style>
-    h1{color:skyblue;}
-    .active{color:red;}
 </style>
