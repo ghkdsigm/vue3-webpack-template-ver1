571dfb053f5bed83d182ef14129a8ffdebf36d15
2022project <ghkdsigm3@gmail.com>
2022-02-03 15:26:03 +0900

    count 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
-    <Hello ref="hello" />
+    <h1 @click="increase">
+        {{ count }} / {{ doubleCount }}
+    </h1>
+    <h1>
+        {{ message }} / {{ reversedMessage }}
+    </h1>
 </template>
 
 <script>
-import Hello from '~/components/hello'
 export default { 
-    components: {
-        Hello
+    data(){
+        return {
+            message: 'Hello world',
+            count:0
+        }
     },
-    mounted(){
-        console.log(this.$refs.hello.$el)
+    computed: {
+        doubleCount(){
+            return this.count * 2
+        },
+        reversedMessage(){
+            return this.message.split('').reverse().join('')
+        }
+    },
+    methods: {
+        increase(){
+            this.count += 1
+        }
     }
+    
 }
 </script>
 
